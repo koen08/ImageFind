644fe9e96c6c60f6634c2e581b6ff1d15ed96dd6
butus <konstantin.butusov2017@yandex.ru>
2022-02-24 15:23:23 +0700

    Added SerializedName for ImageNet

--- app/src/main/java/com/example/imagefind/data/network/models/ImageNet.kt
+++ app/src/main/java/com/example/imagefind/data/network/models/ImageNet.kt
@@ -3,10 +3,10 @@ package com.example.imagefind.data.network.models
 import com.google.gson.annotations.SerializedName
 
 data class ImageNet(
-    val id: Long,
+    @SerializedName("id") val id: Long,
     @SerializedName("webformatURL") val url: String,
-    val userImageURL: String,
+    @SerializedName("userImageURL") val userImageURL: String,
     @SerializedName("user") val userName: String,
-    val likes: Int,
-    val views: Int
+    @SerializedName("likes") val likes: Int,
+    @SerializedName("views") val views: Int
 )
